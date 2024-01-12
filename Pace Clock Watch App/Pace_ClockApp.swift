e72412d85d15b78fc8117bfdaf1c6e674253f814
ZTW21 <ztwilson2002@gmail.com>
2024-01-12 11:22:09 -0500

    Initial Commit

---
+++ Pace Clock Watch App/Pace_ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pace_ClockApp.swift
+//  Pace Clock Watch App
+//
+//  Created by Zack Wilson on 1/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Pace_Clock_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
